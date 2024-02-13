34eb825c432507a6ec7cdde9322e00c82e869e94
Johanbjorkoy <johan.bjorkoy@icloud.com>
2024-02-13 13:48:04 +0100

    endring

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function tilfeldigKort() {
+function tilfeldigkort() {
 
-    const tall = Math.floor(Math.random() * 13) + 1
+    const tall = Math.floor(Math.random()* 13) + 1
+    
+    
     const kortElm = document.querySelector(".container img")
+    
+    
     kortElm.src = "kortstokk/" + tall + "_of_clubs.png"
-
-}
-
-
-
-
-setInterval(tilfeldigKort, 1000)
+    
+    const suits = ["spades", "hearts", "clubs", "diamonds"]
+    
+    const suit_index = Math.floor(Math.random()*4 )
+    
+    const suit = suits[suit_index]
+    
+    kortElm.src = "kortstokk/" + tall + "_of_" + suit + ".png"
+    
+    }
+    setInterval(tilfeldigkort, 1000)
+    
